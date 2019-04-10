e4461203792ba4ecc73b29045acb573b4fe9a389
bbennett7 <bryn.bennett@gmail.com>
2019-04-10 13:12:08 -0700

    Add beginning of links to user homepage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   get '/users/:slug' do #show's user home page
     if logged_in?
+      @user.find_by_slug(params[:slug])
       erb :'/users/show'
     else
       redirect '/login'
