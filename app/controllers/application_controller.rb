c7770da14b27e7e7bfb53df365f652ce3c21e701
bbennett7 <bryn.bennett@gmail.com>
2019-04-10 12:10:36 -0700

    Built sign up form, add flash messages, add slug capabilities to username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 require './config/environment'
+require 'rack-flash'
 
 class ApplicationController < Sinatra::Base
-
+  use Rack::Flash
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
@@ -10,7 +11,28 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    #shows homepage with login and signup
     erb :index
+  end
+
+  get '/signup' do
+    if logged_in?
+      #redirect to user homepage
+    else
+      #shows signup page
+      erb :'/users/create_user'
+    end
+  end
+
+  post '/signup' do
+    if !params[:name].empty? && !params[:email].empty? && !params[:username].empty? && !params[:passowrd].empty?
+      @user = User.create(params) #creates user
+      session[:user_id] = @user.id #log user in
+    #redirect #user homepage
+    else
+      flash[:message] = "Error: All fields are required for signup."
+      redirect '/signup'
+    end
   end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,14 @@ class User < ActiveRecord::Base
 
   has_many :experiences
   has_many :locations, through: :experiences
+
+  def slug
+    self.username.downcase.gsub(/[^0-9a-z\- ]/, "").gsub(" ", "-")
+  end
+
+  def self.find_by_slug(username)
+    user = User.find{|user| user.slug == username}
+  end
 end
 
-#A user has an email, password, name, and username, has many experiences and has many locations through experiences 
+#A user has an email, password, name, and username, has many experiences and has many locations through experiences
