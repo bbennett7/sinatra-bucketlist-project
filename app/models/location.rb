34c77c0c19088d2439fc390552f79605279397f6
bbennett7 <bryn.bennett@gmail.com>
2019-04-11 15:33:50 -0700

    Added slug capabilities to location

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,14 @@
 class Location < ActiveRecord::Base
   has_many :experiences
+
+  def slug
+    full_name = "#{Self.city} #{Self.country}"
+    self.fullname.downcase.gsub(/[^0-9a-z\- ]/, "").gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    location = Location.find{|location| location.slug == location}
+  end
 end
 
-#A location has a name(city/country), and has many experiences 
+#A location has a name(city/country), and has many experiences
