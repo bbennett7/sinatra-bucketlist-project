cf4b8e7ab4030c9437847147d720645178098f3c
bbennett7 <bryn.bennett@gmail.com>
2019-07-08 19:20:16 -0700

    update production info in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -6,9 +6,24 @@ ENV['SINATRA_ENV'] ||= "bucketlistapp_development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "postgresql",
-  :database => "#{ENV['SINATRA_ENV']}"
-)
+configure :development do
+  ActiveRecord::Base.establish_connection(
+    :adapter => "sqlite3",
+    :database => "#{ENV['SINATRA_ENV']}"
+  )
+end
+
+configure :production do
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+
+  ActiveRecord::Base.establish_connection(
+    :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+    )
+end
 
 require_all 'app'
