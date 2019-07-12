71bc110deec5f8772a45dc81358b8db3869d17fc
bbennett7 <bryn.bennett@gmail.com>
2019-07-12 08:40:50 -0700

    fix map display bug

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,5 +1,6 @@
 class LocationsController < ApplicationController
   get '/travels/:slug/map' do
+    @user = current_user
     @location = Location.find_by_slug(params[:slug])
 
     erb :'/locations/map'
